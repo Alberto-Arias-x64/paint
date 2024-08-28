94b1625623e9503fb214c3f11c6bf41e61e2176b
Alberto Arias <alberto.arias.x64@gmail.com>
2024-08-28 11:35:36 -0500

    simple paint

---
+++ paint.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
+const color = document.querySelector('#color-picker')
+const MODES = {
+  DRAW: 'draw',
+  ERASE: 'erase',
+  FILL: 'fill',
+  RECT: 'rect',
+  CIRCLE: 'circle',
+  LINE: 'line'
+}
+
+let isDrawing = false
+let startX = 0
+let startY = 0
+let lastX = 0
+let lastY = 0
+let mode = MODES.DRAW
+
+canvas.addEventListener('mousedown', ({ offsetX, offsetY }) => {
+  isDrawing = true
+  ;[lastX, lastY] = [offsetX, offsetY]
+})
+
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false
+})
+
+canvas.addEventListener('mouseleave', () => {
+  isDrawing = false
+})
+
+canvas.addEventListener('mousemove', ({ offsetX, offsetY }) => {
+  if (!isDrawing) return
+  ctx.beginPath()
+  ctx.moveTo(lastX, lastY)
+  ctx.lineTo(offsetX, offsetY)
+  ctx.strokeStyle = color.value
+  ctx.lineWidth = 2
+  ctx.stroke()
+  ;[lastX, lastY] = [offsetX, offsetY]
+})
